1a3f6678f57cb4112bb01b969d946d2fdcbd6c7f
Zair <loboser.sl@gmail.com>
2022-05-21 11:01:50 -0300

    inverso

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+namespace tp05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, inverso = 0;
+
+			Console.WriteLine("Ingrese un numero: ");
+			numero = Convert.ToInt32(Console.ReadLine());
+
+			if (numero > 0)
+			{
+				while (numero > 0)
+				{
+					inverso = inverso * 10 + numero % 10;
+					numero = numero / 10;
+				}
+			}
+			else
+			{
+				inverso = numero;
+			}
+
+			Console.WriteLine("Numero invertido: " + inverso);
+        }
+    }
+}
