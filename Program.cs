fabfb26aa9e7da6e91e3728cf4fe22ee3d93fbb1
Zair <loboser.sl@gmail.com>
2022-05-21 11:45:31 -0300

    avance ejercicio4

--- Program.cs
+++ Program.cs
@@ -6,25 +6,34 @@ namespace tp05
     {
         static void Main(string[] args)
         {
-            int numero, inverso = 0;
+            int longitudDeCadena;
+            string cadena1, cadena2;
+            int resultado;
 
-			Console.WriteLine("Ingrese un numero: ");
-			numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese una cadena de caracteres: ");
+            cadena1 = Console.ReadLine();
+            longitudDeCadena = cadena1.Length;
 
-			if (numero > 0)
-			{
-				while (numero > 0)
-				{
-					inverso = inverso * 10 + numero % 10;
-					numero = numero / 10;
-				}
-			}
-			else
-			{
-				inverso = numero;
-			}
 
-			Console.WriteLine("Numero invertido: " + inverso);
+            cadena2 = "Algunas letras de la cadena: ";
+
+            for (int i = 0; i < longitudDeCadena; i++)
+            {
+                cadena2 += cadena1[i] + " ";
+                i+= 2;
+            }
+
+            Console.WriteLine(cadena2);
+            Console.WriteLine("Longitud de la cadena: " + longitudDeCadena);
+
+            Console.WriteLine("\nIngrese otra cadena de caracteres: ");
+            cadena2 = Console.ReadLine();
+
+            cadena1 = String.Concat(cadena1,cadena2);
+            Console.WriteLine("\nCadenas Concatenadas: " + cadena1);
+            
+            cadena1 = cadena1.Substring(0, longitudDeCadena);
+            Console.WriteLine("\nSubcadena: " + cadena1);
         }
     }
 }
